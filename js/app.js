81adc8c1a6903d60ee062b23b5dee628682e4ebe
Daniel Diaz <diazjfdaniel@gmail.com>
2015-03-31 13:26:41 -0700

    Added functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 var main = function() {
     /* Push the body and the nav over by 285px over */
+    var open = false;
     $('.icon-menu').click(function() {
         $('.menu').animate({
             left: "0px"
         }, 200);
+        open = false;
 
     });
 
@@ -12,6 +14,32 @@ var main = function() {
         $('.menu').animate({
             left: "-285px"
         }, 200);
+        open = true;
+    });
+
+    $('body').click(function() {
+        if (open) {
+            $('.icon-close').click(function() {
+                $('.menu').animate({
+                    left: "-285px"
+                }, 200);
+                open = false;
+            });
+        }
+    })
+
+    /* Smooth scroll */
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
     });
 };
 
